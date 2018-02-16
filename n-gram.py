c8e2677222ac28ada4eeb6fa14c029ee0354917f
mohanenm <ethannmohan@gmail.com>
2018-02-15 18:36:39 -0600

    minor change, trying to find better way to read files...not going well

--- n-gram.py
+++ n-gram.py
@@ -1,16 +1,22 @@
-import nltk
-from nltk.corpus import reuters
+ import nltk
+ from nltk.corpus import reuters
 
 
-reuters
 
 
 def format_sentence(sent):
      return {word: True for word in nltk.word_tokenize(sent)}
 
 
-corpus = []
-with open("reuters-corpus") as p:
+def collection_stats():
+    # List of documents
+    documents = reuters.fileids()
+    print(str(len(documents)) + " documents");
+
+    id = category_docs[0]
+    words =
+
+with read(id) a:
       for words in p:
           corpus.append([format_sentence(words), 'corpus'])
 

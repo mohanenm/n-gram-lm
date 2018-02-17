6154415bd3e04f27ce6ae59d262b75c8fcffbe74
mohanenm <ethannmohan@gmail.com>
2018-02-17 02:43:47 -0600

    minor-change

--- n-gram.py
+++ n-gram.py
@@ -9,7 +9,6 @@ filterTwo = (re.sub('[@#*()]', "", filterOne))
 filterThree = (filterTwo.replace("SCENE", " "))
 filterFour = (filterThree.replace("ACT", " "))
 finalData = filterFour
-
 '''
 def format_sentence(sent):
     return {word: True for word in nltk.word_tokenize(sent)}
@@ -20,7 +19,6 @@ with open("sdata.txt") as p:
     for words in p:
         seg.append([format_sentence(words), 'seg'])
 '''
-
 tokens = nltk.word_tokenize(finalData)
 bigram = nltk.ngrams(tokens, 2)
 print(collections.Counter(bigram))

35e2dbb0bc5813a3b3dae37c18ab8304c94479c8
mohanenm <ethannmohan@gmail.com>
2018-02-17 04:47:05 -0600

    slowly, slowly getting somewhere

--- n-gram.py
+++ n-gram.py
@@ -2,27 +2,31 @@ import nltk
 from nltk.probability import *
 import re
 import collections
+from collections import Counter
 
+''' cleaning data'''
 data = open("sdata.txt", 'r').read()
 fOne = ''.join(filter(lambda x: not x.isdigit(), data))
-fTwo = (re.sub('[@#*%;()}]', "", fOne))
+fTwo = (re.sub('[-,_[@#*?!.""%;()}]', " ", fOne))
 fThree = (fTwo.replace("SCENE", " "))
 fFour = (fThree.replace("ACT", " "))
 finalData = fFour
+
+'''tokenization + nltk bigram,trigram generation 
+
 tokens = nltk.word_tokenize(finalData)
 bigram = nltk.ngrams(tokens, 2)
-collections.Counter(bigram)
+Counter(bigram)
 trigram = nltk.ngrams(tokens, 3)
-collections.Counter(trigram)
+Counter(trigram)
 
-total = 0
-count = collections.defaultdict(int)
-for word in finalData:
-    total += 1
-    count[word] += 1
-for word, ct in count.items():
-     print([word, 100.0 * int(ct) / int(total)])
+'''
 
+wordcount = Counter(finalData.split())
+for item in wordcount.items():
+    print("{}\t{}".format(*item))
+
+print(len(finalData.split()))
 
 
 

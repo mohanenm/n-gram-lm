3cf84c5035a441c4d57f2f0b0024c1359ba75297
mohanenm <ethannmohan@gmail.com>
2018-02-17 04:00:04 -0600

    dont even know anymore

--- n-gram.py
+++ n-gram.py
@@ -1,28 +1,27 @@
 import nltk
+from nltk.probability import *
 import re
 import collections
 
-
 data = open("sdata.txt", 'r').read()
-filterOne = ''.join(filter(lambda x: not x.isdigit(), data))
-filterTwo = (re.sub('[@#*()]', "", filterOne))
-filterThree = (filterTwo.replace("SCENE", " "))
-filterFour = (filterThree.replace("ACT", " "))
-finalData = filterFour
-'''
-def format_sentence(sent):
-    return {word: True for word in nltk.word_tokenize(sent)}
-
-
-seg = []
-with open("sdata.txt") as p:
-    for words in p:
-        seg.append([format_sentence(words), 'seg'])
-'''
+fOne = ''.join(filter(lambda x: not x.isdigit(), data))
+fTwo = (re.sub('[@#*%;()}]', "", fOne))
+fThree = (fTwo.replace("SCENE", " "))
+fFour = (fThree.replace("ACT", " "))
+finalData = fFour
 tokens = nltk.word_tokenize(finalData)
 bigram = nltk.ngrams(tokens, 2)
-print(collections.Counter(bigram))
+collections.Counter(bigram)
+trigram = nltk.ngrams(tokens, 3)
+collections.Counter(trigram)
 
+total = 0
+count = collections.defaultdict(int)
+for word in finalData:
+    total += 1
+    count[word] += 1
+for word, ct in count.items():
+     print([word, 100.0 * int(ct) / int(total)])
 
 
 

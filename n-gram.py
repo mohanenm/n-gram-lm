9003c3f6cbc5393220291785bd255e831ccee818
mohanenm <ethannmohan@gmail.com>
2018-02-18 17:41:19 -0600

    minor-updates

--- n-gram.py
+++ n-gram.py
@@ -1,11 +1,10 @@
 import re
 from collections import Counter
 import nltk
-from nltk import sent_tokenize
+from nltk import sent_tokenize, word_tokenize
 from nltk import ngrams
 from nltk import FreqDist
 import itertools
-
 ''
 'cleaning data'
 ''
@@ -19,13 +18,11 @@ finalData = fFour
 ''' 
   tokenization + nltk bigram,trigram generation 
 
-tokens = nltk.word_tokenize(finalData)
-bigram = nltk.ngrams(tokens, 2)
-Counter(bigram)
-trigram = nltk.ngrams(tokens, 3)
-Counter(trigram)
-
 '''
+
+tokens = word_tokenize(finalData)
+bigram = nltk.ngrams(tokens, 2)
+print(bigram)
 
 wordcount = Counter(finalData.split())
 for item in wordcount.items():
@@ -35,13 +32,11 @@ for item in wordcount.items():
 'total word count: 963989'
 ''
 print(len(finalData.split()))
-
 ''
-'segmenting text'
+' sentence segmenting'
 ''
 sent_tokenize_list = sent_tokenize(finalData)
-
+print(sent_tokenize)
 bigram = ngrams(sent_tokenize(finalData), 2)
-print(bigram)
 fdist = FreqDist(itertools.chain(bigram))
 print(fdist)

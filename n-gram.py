289069442b02139a706117f554d446f6bfda61ca
mohanenm <ethannmohan@gmail.com>
2018-03-08 03:37:35 -0600

    minor b change

--- n-gram.py
+++ n-gram.py
@@ -14,37 +14,29 @@ fTwo = (re.sub('[0-9\W]+', " ", data))
 fThree = (fTwo.replace("SCENE", " "))
 fFour = (fThree.replace("ACT", " "))
 finalData = fFour
-
 ''' 
   tokenization + nltk bigram,trigram generation 
 
 '''
-
 tokens = word_tokenize(finalData)
-
 wordcount = Counter(finalData.split())
 for item in wordcount.items():
    ("{}\t{}".format(*item))
-
-for k,v in wordcount.items():
-    print(k, v)
-
+totalWordcount = sum(wordcount.values())
 ''
 'total word count: 963989'
 ''
 ''
 ' sentence segmenting'
 ''
-'''
 sent_tokenize_list = sent_tokenize(finalData)
 print(sent_tokenize)
 bigram = list(nltk.bigrams(finalData.split()))
 print(*map(' '.join, bigram), sep=',')
 wordCounter = Counter(bigram)
 for k,v in wordCounter.items():
-    final = v/963989
+    final = v/totalWordcount
 
     print(final)
 
 
-'''

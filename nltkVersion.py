630b4813ffa17dab87ac3d80bcb65297a720e43a
mohanenm <ethannmohan@gmail.com>
2018-03-10 17:55:44 -0600

    nltk version, bag of words

--- nltkVersion.py
+++ nltkVersion.py
@@ -1,10 +1,30 @@
 
-from nltk.corpus import brown
-from nltk.probability import LidstoneProbDist
+from nltk.corpus import reuters
+from nltk import bigrams, trigrams
+from collections import Counter, defaultdict
 
+i_counter = Counter(reuters.words())
+t_count = len(reuters.words())
 
-estimator = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
-lm = NgramModel(2, brown.words(categories='news'), estimator)
+print(i_counter.most_common(n=10))
 
-print(lm.prob("word", ["This is a context which generates a word"]))
+for word in i_counter:
+    i_counter[word]/= int(t_count)
 
+    print(sum(i_counter.values()))
+
+    import random
+
+    text_rand = []
+
+    for _ in range(100):
+        r = random.random()
+        counter_rand = .0
+
+        for word, freq in i_counter.items():
+            counter_rand += freq
+
+            if counter_rand > r:
+                text_rand.append(word)
+                break
+print(' '.join(text_rand))

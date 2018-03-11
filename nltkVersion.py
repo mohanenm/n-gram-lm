fd4015b03b6e197f441bbaf32f521436315df03f
mohanenm <ethannmohan@gmail.com>
2018-03-10 22:02:32 -0600

    finally wrapping up

--- nltkVersion.py
+++ nltkVersion.py
@@ -1,16 +1,18 @@
-from nltk import word_tokenize
+import nltk
+from nltk import word_tokenize, sent_tokenize
 from nltk import bigrams, trigrams
 from collections import Counter, defaultdict
 from operator import mul
 
 import re
-data = open("westernPhil.txt", 'r').read()
+
+data = open("westPhil.txt", 'r').read()
 fTwo = (re.sub(',[0-9\W]+', " ", data))
 fThree = (fTwo.replace("SCENE", " "))
 fFour = (fThree.replace("ACT", " "))
 final_data = fFour
 tokens = word_tokenize(final_data)
-tokens_final =[item.lower() for item in tokens]
+tokens_final = [item.lower() for item in tokens]
 i_counter = Counter(tokens_final)
 t_count = len(tokens_final)
 
@@ -21,19 +23,40 @@ for word in i_counter:
     import random
     from functools import reduce
 
-
 text_rand = []
 for _ in range(10):
-        r = random.random()
-        counter_rand = .0
+    r = random.random()
+    counter_rand = .0
 
-        for word, freq in (i_counter.items()):
-            counter_rand += freq
+    for word, freq in (i_counter.items()):
+        counter_rand += freq
 
-            if counter_rand > r:
-                text_rand.append(word)
-                break
+        if counter_rand > r:
+            text_rand.append(word)
+            break
 
 print(' '.join(text_rand))
 
 print(reduce(mul, [i_counter[w] for w in text_rand], 1.0))
+
+bigram_sentence = nltk.ngrams(tokens_final, 2, pad_left=True, pad_right=True)
+
+print(bigram_sentence)
+
+lm_model = defaultdict(lambda: defaultdict(lambda: 0))
+
+for w1, w2 in bigram_sentence:
+    lm_model[w1][w2] += 1
+
+print(lm_model['it']["is"])
+'''test'''
+print(lm_model["it"]["isubv"])
+
+for w1 in lm_model:
+    t_count = int(sum(lm_model[w1].values()))
+    for w3 in lm_model[w1]:
+        lm_model[w1][w2] /= t_count
+
+print(lm_model['it']["is"])
+print(lm_model["it"]["isubv"])
+print(lm_model[None, None]["The"])
